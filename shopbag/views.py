bc8e47e3efa727f8bcf61d336abeeb0ab28a4fd9
Markp1312 <mpg.peeters3@gmail.com>
2022-03-27 12:54:00 +0000

    Created view bag view and bag.html

--- shopbag/views.py
+++ shopbag/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_bag(request):
+    """ A view that shows the bag content page """
+
+    return render(request, 'bag/bag.html')
