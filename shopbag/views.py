ad068bd85397cab6c5b17678aef54ad33a85032c
Markp1312 <mpg.peeters3@gmail.com>
2022-04-01 15:10:21 +0000

    Added adjust_bag view

--- shopbag/views.py
+++ shopbag/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import (
-    render, redirect, reverse, HttpResponse, get_object_or_404
-)
+from django.shortcuts import render, redirect, reverse
+
 
 # Create your views here.
 
@@ -24,3 +23,18 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     # print(request.session['bag'])
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    # print(request.session['bag'])
+    return redirect(reverse('view_bag'))
