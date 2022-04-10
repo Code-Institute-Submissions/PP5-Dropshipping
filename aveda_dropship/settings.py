7c63b393a126879b8b387b83e8474574beefddec
Markp1312 <mpg.peeters3@gmail.com>
2022-04-10 09:34:23 +0000

    Updated settings.py builtins

--- aveda_dropship/settings.py
+++ aveda_dropship/settings.py
@@ -82,8 +82,8 @@ TEMPLATES = [
                 'shopbag.contexts.bag_contents',
             ],
             'builtins': [
-               'crispy_forms.templatetags.crispy_forms_tags'
-               'crispy_forms.templatetags.crispy_forms_field'
+               'crispy_forms.templatetags.crispy_forms_tags',
+               'crispy_forms.templatetags.crispy_forms_field',
             ]
         },
     },
