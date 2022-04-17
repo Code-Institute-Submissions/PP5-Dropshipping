698efe69b883898d5303c672f7c01a2893ff0ac4
Markp1312 <mpg.peeters3@gmail.com>
2022-04-17 17:50:06 +0000

    Removed secret key and set debug

--- aveda_dropship/settings.py
+++ aveda_dropship/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wn1fw-i5uf)k3aicrjs)_u&1f_-o*3^0xzyn-e2uj@fz=nkmek'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['markdropshippingaveda.herokuapp.com','localhost']
 
